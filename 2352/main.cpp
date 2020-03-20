7d539d56404143326964260e3c9a437fc236ad0a
hahaha <123@123.com>
2020-03-20 22:53:01 +0800

    new

--- 2352/main.cpp
+++ 2352/main.cpp
@@ -1,37 +1,41 @@
 #include <cstdio>
 #define maxn 32002
 using namespace std;
-int c[maxn],ans[15002];
+int c[maxn], ans[15002];
 int lowbit(int x)
 {
-	return x&(-x);
+	return x & (-x);
 }
 int getsum(int n)
 {
-	int sum=0;
-	while(n>0) {
-		sum+=c[n];
-		n-=lowbit(n);
+	int sum = 0;
+	while (n > 0)
+	{
+		sum += c[n];
+		n -= lowbit(n);
 	}
 	return sum;
 }
-void update(int pos,int num)
+void update(int pos, int num)
 {
-	while(pos<maxn) {
-		c[pos]+=num;
-		pos+=lowbit(pos);
+	while (pos < maxn)
+	{
+		c[pos] += num;
+		pos += lowbit(pos);
 	}
 }
 int main()
 {
 	int n;
-	scanf("%d",&n);
-	for(int i=0; i<n; i++) {
-		int a,b;
-		scanf("%d%d",&a,&b);
-		ans[getsum(a+1)]++;
-		update(a+1,1);
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		scanf("%d%d", &a, &b);
+		ans[getsum(a + 1)]++;
+		update(a + 1, 1);
 	}
-	for(int i=0; i<n; i++) printf("%d\n",ans[i]);
+	for (int i = 0; i < n; i++)
+		printf("%d\n", ans[i]);
 	return 0;
 }
